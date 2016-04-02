680c58e737b3feb57338d508f10415c4483874cb
Michael Knop <micknop@t-online.de>
2016-04-02 09:20:03 +0200

    Bis Seite 262

--- db_create.py
+++ db_create.py
@@ -1,13 +1,15 @@
-from views import db
-from models import Task
 from datetime import date
+from project import db
+from project.models import Task, User
 
 # create the database and the db table
 db.create_all()
 
 # insert data
-db.session.add(Task("Finish this tutorial", date(2015, 3, 13), 10, 1))
-db.session.add(Task("Finish Real Python", date(2015, 3, 13), 10, 1))
+db.session.add(User("admin", "ad@min.com", "admin", "admin"))
+
+db.session.add(Task("Finish this tutorial", date(2015, 3, 13), 10, date(2015, 3, 13), 1, 1))
+db.session.add(Task("Finish Real Python", date(2015, 3, 13), 10, date(2015, 3, 13), 1, 1))
 
 # commit the changes
 db.session.commit()    
